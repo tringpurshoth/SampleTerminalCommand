0bd57041d555a94e5c87f7bcb7c339e2c417d1e5
tringpurshoth <purshothaman.k@tringapps.com>
2015-05-02 10:51:54 +0530

    image added

---
+++ SampleImage/SampleImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleImage
+//
+//  Created by purshothaman-ipad on 02/05/15.
+//  Copyright (c) 2015 purshothaman-ipad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SampleImage/SampleImage/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SampleImage
+//
+//  Created by purshothaman-ipad on 02/05/15.
+//  Copyright (c) 2015 purshothaman-ipad. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
